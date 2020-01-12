62f1f1a04d09766a327becece179812075609418
Edgar Lugo <elu.developer@gmail.com>
2020-01-12 23:45:30 +0100

    add suport for single pokemons

--- src/js/components/searchbar.js
+++ src/js/components/searchbar.js
@@ -6,12 +6,27 @@ class SearchBar extends Component {
         e.preventDefault()
         const input = document.getElementById("search-input")
         
+        input.value = input.value.toLowerCase()
+
         if (!input.value) return
 
-        fetch(`https://pokeapi.co/api/v2/type/${input.value}`)
-            .then(res => res.json()
-                .then(data => this.props.setPokemons(data.pokemon))
-                .catch(console.log))
+        const types = ["normal", "fire", "fighting", "water", "flying", "grass", "poison", "electric", "ground", "psychic", "rock", "ice", "bug", "dragon", "ghost", "dark", "steel", "fairy"]
+        
+        if (types.includes(input.value)) {
+            fetch(`https://pokeapi.co/api/v2/type/${input.value}`)
+                .then(res => res.json()
+                    .then(data => this.props.setPokemons(data.pokemon))
+                    .catch(console.log))
+        } else {
+            fetch(`https://pokeapi.co/api/v2/pokemon/${input.value}`)
+                .then(res => res.json()
+                    .then(data => {
+                        const poke = {name: data.name}
+                        poke.url = `/${data.id}/`
+
+                        this.props.setPokemons([{pokemon:poke}])
+                    }).catch(console.log))
+        }
 
         input.value = ''
     }
