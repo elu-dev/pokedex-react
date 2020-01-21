6e1cf8eea6a0a86c23fd64682f3deb3d068b7f21
Edgar Lugo <elu.developer@gmail.com>
2020-01-21 01:44:50 +0100

    Updates

---
+++ precache-manifest.416d708bd5fe701bb8beae405db1cd31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50b2259044703acdc0c502f416289d40",
+    "url": "/react-pokedex/index.html"
+  },
+  {
+    "revision": "61fffb88c0764263068e",
+    "url": "/react-pokedex/static/css/main.479ed182.chunk.css"
+  },
+  {
+    "revision": "c180b9af3b46ba5a072e",
+    "url": "/react-pokedex/static/js/2.2e529fee.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/react-pokedex/static/js/2.2e529fee.chunk.js.LICENSE"
+  },
+  {
+    "revision": "61fffb88c0764263068e",
+    "url": "/react-pokedex/static/js/main.16813559.chunk.js"
+  },
+  {
+    "revision": "a0fa66ac64f628a05b67",
+    "url": "/react-pokedex/static/js/runtime-main.303dafe9.js"
+  }
+]);
